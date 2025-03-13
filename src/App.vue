934634fbcbca825c0b9811022260dd3f6d643623
EafenQAQ <eafenqaq@gmail.com>
2025-03-13 15:59:09 +0800

    add header

--- src/App.vue
+++ src/App.vue
@@ -1,47 +1,23 @@
-<script setup>
-import HelloWorld from './components/HelloWorld.vue'
-import TheWelcome from './components/TheWelcome.vue'
-</script>
-
 <template>
-  <header>
-    <img alt="Vue logo" class="logo" src="./assets/logo.svg" width="125" height="125" />
-
-    <div class="wrapper">
-      <HelloWorld msg="You did it!" />
-    </div>
-  </header>
-
-  <main>
-    <TheWelcome />
+  <main class="font-Raleway flex flex-col items-center justify-center">
+    <header class="w-full">
+      <div class="bg-gray-200 flex flex-1 w-full items-center h-[125px] gap-5 justify-center">
+        <img 
+          class="max-w-[60px] rotate-350"
+          src="./assets/pinia-logo.svg" alt="Logo">
+        <h1 class="text-5xl rotate-2 font-semibold font-fragility text-gray-500">Pinia todo</h1>
+      </div>
+    </header>
+   
   </main>
 </template>
 
-<style scoped>
-header {
-  line-height: 1.5;
-}
+<script setup>
+import { useTaskStore } from './stores/todoStores';
 
-.logo {
-  display: block;
-  margin: 0 auto 2rem;
-}
+const taskStore = useTaskStore();
+</script>
 
-@media (min-width: 1024px) {
-  header {
-    display: flex;
-    place-items: center;
-    padding-right: calc(var(--section-gap) / 2);
-  }
+<style>
 
-  .logo {
-    margin: 0 2rem 0 0;
-  }
-
-  header .wrapper {
-    display: flex;
-    place-items: flex-start;
-    flex-wrap: wrap;
-  }
-}
 </style>
